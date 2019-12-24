98cf41d1ba64d926f52d7e100131ca7ffe49e7d5
Ahmed Korim <ahmedkorrim@gmail.com>
2019-12-24 16:10:30 +0200

    Update index.js
    support gzip files

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const Express = require( 'express' );
 const path = require( 'path' );
 const fs = require( 'fs' )
+const expressStaticGzip = require( 'express-static-gzip' );
 const app = Express()
 
-app.use( Express.static( path.join( __dirname, '..', 'build' ) ) )
-app.get( '*', ( req, res ) => {
-    const fileStream = fs.createReadStream( path.join( __dirname, '..', 'build', 'index.html' ) )
-    res.contentType( 'text/html' )
-    fileStream.pipe( res )
-} )
+// app.get( '*', ( req, res ) => {
+//     const fileStream = fs.createReadStream( path.join( __dirname, '..', 'build', 'index.html' ) )
+//     res.contentType( 'text/html' )
+//     fileStream.pipe( res )
+// } )
+// app.use( Express.static( path.join( __dirname, '..', 'build' ) ) )
+app.use( '/', expressStaticGzip( path.join( __dirname, '..', 'build' ), {
+    enableBrotli: true,
+    customCompressions: [ {
+        encodingName: 'deflate',
+        fileExtension: 'zz'
+    } ],
+    orderPreference: [ 'br' ]
+} ) );
+app.use( '*', ( req, res, next ) => {
+    req.url = '/';
+    return expressStaticGzip( path.join( __dirname, '..', 'build' ), {
+        enableBrotli: true,
+        customCompressions: [ {
+            encodingName: 'deflate',
+            fileExtension: 'zz'
+        } ],
+        orderPreference: [ 'br' ]
+    } )( req, res, next );
+} );
 app.listen( 8055 )
